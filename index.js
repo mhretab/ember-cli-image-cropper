9c988266277e079f2c5806444de75bc8860a2177
Mhretab Kidane <mhretabk@gmail.com>
2015-03-30 15:47:54 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/* jshint node: true */
+'use strict';
+
+module.exports = {
+  name: 'ember-cli-cropper',
+
+  included: function(app) {
+      this._super.included(app);
+
+      app.import(app.bowerDirectory + '/cropper/dist/cropper.min.js');
+      app.import(app.bowerDirectory + '/cropper/dist/cropper.min.css');
+  }
+};
