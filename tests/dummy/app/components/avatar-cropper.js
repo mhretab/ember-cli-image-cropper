e00ec3de9204785d551e3605dbc6e4fd8a9503f2
Mhretab Kidane <mhretabk@gmail.com>
2015-05-01 21:54:34 +0200

    created a dummy app for demo

---
+++ tests/dummy/app/components/avatar-cropper.js
@@ -0,0 +1,20 @@
+import imageCropper from 'ember-cli-image-cropper/components/image-cropper';
+
+export default imageCropper.extend({
+  //override default options of cropper
+  aspectRatio: 1,
+  minCropBoxWidth: 100,
+  minCropBoxHeight: 100,
+  cropperContainer: '.cropper-container > img',
+  previewClass: '.img-preview',
+  croppedAvatar: null,
+
+  actions: {
+    getCroppedAvatar: function() {
+      var container = this.$(this.get('cropperContainer'));
+      var croppedImage = container.cropper('getCroppedCanvas');
+      this.set('croppedAvatar', croppedImage);
+      Ember.Logger.warn(croppedImage);
+    }
+  }
+});
