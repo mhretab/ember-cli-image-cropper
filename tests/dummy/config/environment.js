7dc1040a99a26d3857a8841ec999e48b658eac60
Mhretab Kidane <mhretabk@gmail.com>
2015-05-02 02:06:28 +0200

    changed based url of the dummy app

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -20,6 +20,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+    ENV.baseURL = '/ember-cli-image-cropper';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
