a146068c8e616a650c879b94b71b8305c7ea8d6b
Mhretab Kidane <mhretabk@gmail.com>
2017-02-21 23:18:22 +0100

    upgraded ember-cli and ember

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
-import Resolver from 'ember/resolver';
-import loadInitializers from 'ember/load-initializers';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 var App;
@@ -8,9 +8,9 @@ var App;
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+    modulePrefix: config.modulePrefix,
+    podModulePrefix: config.podModulePrefix,
+    Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
